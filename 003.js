3e3edd9fcbaf8c21c82705052e3555bfc92ad168
VictorMont <victormonteir2@gmail.com>
2020-09-21 14:15:08 -0300

    first commit

---
+++ 003.js
@@ -0,0 +1,5 @@
+console.log('Vai começar');
+for(var c = 0; c <= 10; c++){ //incremento de variavel; condição; incremento de repetição
+    console.log('numero ' + c)
+}
+console.log('FIM...')
---
+++ tabuada.js
@@ -0,0 +1,9 @@
+var numero = 5;
+var c = 0
+if(numero=5){//coloquei o if pensando em fazer a tabuada de acordo com o que foi digitado
+    while(c<=10){
+        var resultado = numero*c;
+        console.log('entao '+numero+' vezes '+c+' é igual a '+ resultado);
+        c++
+    }
+}
